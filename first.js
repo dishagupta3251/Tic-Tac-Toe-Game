52051262807499dc9b44184cdf42e30004301833
Disha <dishaguptalko@gmail.com>
2024-05-17 14:00:53 +0530

    commit

--- first.js
+++ first.js
@@ -1,7 +1,7 @@
 let box=document.querySelectorAll(".box");
 let turn="o";
 const arr=[[0,1,2],[3,4,5],[6,7,8],[0,3,6], [1,4,7] ,[2,5,8] ,[0,4,8] ,[2,4,6]];
-
+let main=document.getElementById("game")
 box.forEach((box1)=>{
     box1.addEventListener("click",()=>{
         if(turn==="o"){
@@ -12,12 +12,31 @@ box.forEach((box1)=>{
             turn="o";
         }
         box1.disabled=true;
+        console.log("box is clicked");
         checkwinner();
+        
     })
 });
 
 const  checkwinner = () => {
-    for(let i=0 ;i<arr.length;i++){
-        console.log(box[arr[i][0]]===box[arr[i][1]]===box[arr[i][2]]);
+    for(let ar of arr){
+    
+        
+        if(box[ar[0]].innerText !=""&&box[ar[1]].innerText!=""&&box[ar[2]].innerText!="")
+        {
+            if(box[ar[0]].innerText===box[ar[1]].innerText&&box[ar[1]].innerText===box[ar[2]].innerText)
+              {
+                let win=document.createElement("div")
+                win.style.marginTop='20px'
+                win.style.fontSize='40px'
+                main.append(win);
+                if(box[ar[0]].innerText==='O')
+                win.innerText="PLAYER 1 is WINNER";
+            else
+            win.innerText="PLAYER 2 is WINNER";
+
+
+              }
+        }
     }
 };
